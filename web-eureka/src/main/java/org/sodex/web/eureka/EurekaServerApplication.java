c6200d01e23fc7799731ef502658096cebd8912e
zengf <zengf@certusnet.com.cn>
2019-08-21 16:13:03 +0800

    eureka基本配置

---
+++ web-eureka/src/main/java/org/sodex/web/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package org.sodex.web.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author DF 
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
+
