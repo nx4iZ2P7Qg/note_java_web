43f8804f4f8dfb68c4813aa6992833940a2df715
zengf <zengf@certusnet.com.cn>
2019-08-22 18:41:23 +0800

    gateway模块

---
+++ web-gateway/src/main/java/org/sodex/web/gateway/GateWayApplication.java
@@ -0,0 +1,12 @@
+package org.sodex.web.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayApplication.class, args);
+	}
+}
