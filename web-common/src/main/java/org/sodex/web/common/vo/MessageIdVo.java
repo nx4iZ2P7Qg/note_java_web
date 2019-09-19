3c19b113002dfca203963907e26ef83d8fcb25d5
zengf <zengf@certusnet.com.cn>
2019-09-19 17:56:41 +0800

    隐藏无用菜单项，完成user部分的所有后台接口

---
+++ web-common/src/main/java/org/sodex/web/common/vo/MessageIdVo.java
@@ -0,0 +1,14 @@
+package org.sodex.web.common.vo;
+
+public class MessageIdVo {
+    
+    private Long msgId;
+
+    public Long getMsgId() {
+        return msgId;
+    }
+
+    public void setMsgId(Long msgId) {
+        this.msgId = msgId;
+    }
+}
--- web-sophia/src/main/java/org/sodex/web/sophia/controller/Menstrual.java
+++ web-sophia/src/main/java/org/sodex/web/sophia/controller/Menstrual.java
@@ -1,27 +1,28 @@
 package org.sodex.web.sophia.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.sodex.web.common.vo.MessageIdVo;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class Menstrual {
-    
-    @PostMapping(value="/login")
-    public String login() throws Exception{
+
+    @PostMapping(value = "/user/login")
+    public String login() throws Exception {
         System.out.println("login");
         ObjectMapper mapper = new ObjectMapper();
         Map<String, Object> data = new HashMap<>();
         data.put("token", "super_admin");
         return mapper.writeValueAsString(data);
     }
-    
-    @GetMapping(value="/get_info")
+
+    @GetMapping(value = "/user/get_info")
     public String getUserInfo(@RequestParam String token) throws Exception {
         System.out.println("getUserInfo");
         ObjectMapper mapper = new ObjectMapper();
@@ -33,10 +34,76 @@ public class Menstrual {
         data.put("access", accessArr);
         return mapper.writeValueAsString(data);
     }
-    
-    @GetMapping(value="/message/count")
+
+    @PostMapping(value = "/user/logout")
+    public String logout() {
+        return null;
+    }
+
+    @GetMapping(value = "/user/message/count")
     public int getMessageCount() {
         System.out.println("getMessageCount");
         return 96;
+    }
+
+    @GetMapping(value = "/user/message/init")
+    public String messageInit() throws Exception {
+        System.out.println("message init");
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, List> reMap = new HashMap<>();
+        List<Map> unreadList = new ArrayList<>();
+        Map<String, Object> unread = new HashMap<>();
+        unread.put("title", "今天是中秋前最后一天");
+        unread.put("create_time", "2019-09-12");
+        unread.put("msg_id", 61);
+        unreadList.add(unread);
+        reMap.put("unread", unreadList);
+        
+        List<Map> readList = new ArrayList<>();
+        Map<String, Object> read = new HashMap<>();
+        read.put("title", "已读消息");
+        read.put("create_time", LocalDate.now() + "");
+        read.put("msg_id", 71);
+        readList.add(read);
+        reMap.put("read", readList);
+        Map<String, Object> read2 = new HashMap<>();
+        read2.put("title", "上个月扣税高多");
+        read2.put("create_time", "2018-07-01");
+        read2.put("msg_id", 72);
+        readList.add(read2);
+        
+        List<Map> trashList = new ArrayList<>();
+        Map<String, Object> trash = new HashMap<>();
+        trash.put("title", "辣鸡消息");
+        trash.put("create_time", LocalDate.now() + "");
+        trash.put("msg_id", 81);
+        trashList.add(trash);
+        reMap.put("trash", trashList);
+        return mapper.writeValueAsString(reMap);
+    }
+    
+    @GetMapping(value = "/user/message/content")
+    public String messageContent(@RequestParam("msg_id") Long msgId) throws Exception {
+        System.out.println("messageContent-msgId = " + msgId);
+        return "消息内容";
+    }
+
+    @PostMapping(value = "/user/message/has_read")
+    public boolean messageHasRead(@RequestBody MessageIdVo vo) throws Exception {
+        System.out.println(vo.getMsgId());
+        System.out.println("messageHasRead-vo = " + new ObjectMapper().writeValueAsString(vo));
+        return true;
+    }
+
+    @PostMapping(value = "/user/message/remove_read")
+    public boolean messageRemoveRead(@RequestBody MessageIdVo vo) throws Exception {
+        System.out.println("messageRemoveRead-vo = " + new ObjectMapper().writeValueAsString(vo));
+        return true;
+    }
+
+    @PostMapping(value = "/user/message/restore")
+    public boolean messageRestore(@RequestBody MessageIdVo vo) throws Exception {
+        System.out.println("messageRestore-vo = " + new ObjectMapper().writeValueAsString(vo));
+        return true;
     }
 }
